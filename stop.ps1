2a5c280b5712d3c21c7fdf2fa628002b4b78b1b5
oooryzaaa <oryzaqiara1@gmail.com>
2026-05-04 13:45:19 +0700

    -

--- stop.ps1
+++ stop.ps1
@@ -1,6 +1,6 @@
 # stop.sh
-docker compose  docker-compose-kafka.yml down
+docker compose docker-compose-kafka.yml down
 docker compose  docker-compose-hadoop.yml down
 
 # Jalankan
-.\stop.ps1
+# .\stop.ps1
